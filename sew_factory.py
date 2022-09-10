53edb6544ecac8f2d6066adcd39dd72620a2b7bd
John Abraham <pdv.dev07@outlook.com>
2022-09-10 18:56:57 +0200

    updated code comments, notes and main method for basic user input.

--- sew_factory.py
+++ sew_factory.py
@@ -1,31 +1,29 @@
-# Riddle 1: Machines & Shirts
 '''
-It takes 5 machines, 5 minutes to make 5 t-shirts
+# Riddle 1: Machines & Shirts.
 
-NOTE: Therefore, each machine needs 5 minutes to make 1 t-shirt.
+It takes 5 machines, 5 minutes to make 5 t-shirts. How long 
+will it take 100 machines to make 100 t-shirts?
 
-Question: 
-How long will it take 100 machines to make 100 t-shirts?
+In the answer to this question a different implementation has
+been created, one that takes into account x machines and y
+t-shirts.
 
-Another Question:
-How long will it take x machines to make y t-shirts?
-
-Answer: 
-Its probably best to reverse the question to, "How long
-will it take to make y t-shirts with x machines?" By 
-dividing the number of t-shirts with the number of machines
-you know how many t-shirts can be made per machine, multiplied 
-by the constant of 5 minutes per t-shirt you'll get the total 
-time it takes to create the t-shirts.
-
-NOTE: In this program some additional conditions have been added to 
-make sure that the total time is valid for both uneven and 
-even number of machines/shirts.
+Initially, it seems that by dividing the number of t-shirts with
+the number of machines multiplied by 5 gives the proper answer
+to this question. However, because a different implementation 
+was in mind, added conditions had also to be taken into account, 
+like even and uneven machines and t-shirts and also considering 
+that more machines than t-shirts, leaves some machines out of 
+the production process; a condition was added that uses those 
+machines when they are double the amount of the t-shirts that need
+to be created - demonstrating a possible solution for doubling
+the workload, i.e. creating the t-shirts twice as fast.
 '''
-
 # time in minutes per t-shirt.
 TIMETAKEN_TSHIRT = 5
 
+# calculate the time to create a t-shirt(s) with a given number
+# of machines and t-shirts to create.
 def get_time_to_create_tshirt(m, t):
     if (m >= t):
         # if there are twice the amount of machines, double the workload
@@ -47,8 +45,40 @@ def print_time_to_create(m, t):
     gttct = get_time_to_create_tshirt(m, t)
     print(f"Total time needed for {m} machines to create {t} shirts: {gttct} minutes.")
 
-print("Riddle 1: Machines & Shirts:")
-n_machines = [5, 100, 200, 200, 100, 300, 5]
-n_tshirts = [5, 100, 100, 200, 200, 100, 100]
-for idx, t in enumerate(n_tshirts):
-    print_time_to_create(n_machines[idx], t)
+# custom exception that can be raised when the input is not a number.
+class ValueNotANumberError(ValueError):
+    """Raised when the input is not a number."""
+    def __init__(self, message = "Error: Value is not a number."):
+        self.message = message
+        super().__init__(self.message)
+
+# main method
+print("Sewing Factory (calculator):")
+question_m = "How many machines can be used? "
+question_t = "How many t-shirts need to be made? "
+question = "Quit the program (y) or do another calculation (n)? [y/n] "
+valid_choice = {"yes": True, "y": True, "ye": True, "no": False, "n": False}
+quit = False
+while quit == False:
+    try:        
+        m = input(question_m)
+        if m.isalpha(): raise ValueNotANumberError()     
+        t = input(question_t)   
+        if t.isalpha(): raise ValueNotANumberError()       
+        print_time_to_create(int(m), int(t))
+        choice = input(question).lower()
+        if (choice in valid_choice):
+            quit = valid_choice[choice]    
+        else:
+            raise ValueError("Error: Invalid answer: '%s'." % choice)
+    except ValueError as e:
+       print(e)
+       continue
+
+# example that uses a list of values for machines and t-shirts to 
+# calculate the t-shirt-creation-times in those situations
+
+# n_machines = [5, 100, 200, 200, 100, 300, 5]
+# n_tshirts = [5, 100, 100, 200, 200, 100, 100]
+# for idx, t in enumerate(n_tshirts):
+#     print_time_to_create(n_machines[idx], t)
